7ed5848dade269d2ef25d1af43a7ba1baca65a17
Jey <jeremyalder@hotmail.fr>
2021-04-22 18:15:18 +0200

    minor fix : eslint check

--- js/fancyboxOptions.js
+++ js/fancyboxOptions.js
@@ -16,13 +16,13 @@ export const fancyboxOptions = () => {
 		idleTime: 9999,
 		smallBtn: "auto",
 		baseTpl:
-			'<div class="fancybox-container" role="dialog" tabindex="-1">' +
-			'<div class="fancybox-bg"></div>' +
-			'<div class="fancybox-inner" aria-label=”image closeup view”>' +
-			'<div class="fancybox-toolbar">{{buttons}}</div>' +
-			'<div class="fancybox-navigation">{{arrows}}</div>' +
-			'<div class="fancybox-stage"></div>' +
-			'<div class="fancybox-caption" tabindex="0"><div class="fancybox-caption__body"></div></div>' +
+			"<div class='fancybox-container' role='dialog' tabindex='-1'>" +
+			"<div class='fancybox-bg'></div>" +
+			"<div class='fancybox-inner' aria-label=”image closeup view”>" +
+			"<div class='fancybox-toolbar'>{{buttons}}</div>" +
+			"<div class='fancybox-navigation'>{{arrows}}</div>" +
+			"<div class='fancybox-stage'></div>" +
+			"<div class='fancybox-caption' tabindex='0'><div class='fancybox-caption__body'></div></div>" +
 			"</div>" +
 			"</div>",
 		btnTpl: {
@@ -38,12 +38,12 @@ export const fancyboxOptions = () => {
 				"</button>",
 
 			arrowRight:
-				'<button aria-label="Suivant" data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
-				'<div><svg class="arrowFancy" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g data-name="Layer 2"><g data-name="chevron-right"><rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0"/><path d="M10.5 17a1 1 0 0 1-.71-.29 1 1 0 0 1 0-1.42L13.1 12 9.92 8.69a1 1 0 0 1 0-1.41 1 1 0 0 1 1.42 0l3.86 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-.7.32z"/></g></g></svg></div>' +
+				"<button aria-label='Suivant' data-fancybox-next class='fancybox-button fancybox-button--arrow_right' title='{NEXT}}'>" +
+				"<div><svg class='arrowFancy' width='24px' height='24px' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><g data-name='Layer 2'><g data-name='chevron-right'><rect width='24' height='24' transform='rotate(-90 12 12)' opacity='0'/><path d='M10.5 17a1 1 0 0 1-.71-.29 1 1 0 0 1 0-1.42L13.1 12 9.92 8.69a1 1 0 0 1 0-1.41 1 1 0 0 1 1.42 0l3.86 4a1 1 0 0 1 0 1.4l-4 4a1 1 0 0 1-.7.32z'/></g></g></svg></div>" +
 				"</button>",
 		},
 	});
-	$('[data-fancybox="images"]').fancybox({
+	$("[data-fancybox='images']").fancybox({
 		afterLoad: function (instance, current) {
 			current.$image.attr("alt", current.opts.$orig.find("img").attr("alt"));
 		},
